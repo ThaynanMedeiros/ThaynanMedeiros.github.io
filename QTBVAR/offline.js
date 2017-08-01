02540f8e61a1a414bee16077b8d7519174d38350
Thaynan Medeiros da Silva <thaynan.medeiros@academico.ifrn.edu.br>
2017-08-01 10:07:04 -0300

    JOGO 2

---
+++ QTBVAR/offline.js
@@ -0,0 +1,22 @@
+﻿{
+	"version": 1501589749,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/iplay-sheet0.png",
+		"images/nomejogo-sheet0.png",
+		"images/iinfo-sheet0.png",
+		"images/iconfiguracao-sheet0.png",
+		"images/ihomevitoria-sheet0.png",
+		"images/quarteirao-sheet0.png",
+		"images/personagem-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
